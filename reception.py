a13cea68bb8a897a516a73a13e225ce2feeef23a
Alexander <alexslabkin@gmail.com>
2019-11-05 18:25:25 +0300

    Add reception part

---
+++ reception.py
@@ -0,0 +1,113 @@
+"""
+
+
+
+"""
+
+import socket
+import struct
+
+# Setup socket object
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5000
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+packets = []
+nals = []
+fudata = b''
+print("Starting socket on", UDP_IP,":",UDP_PORT,end='\n')
+
+while True:
+    try:
+        data = sock.recvfrom(65565)
+    except socket.timeout:
+        print("Packets received:",len(packets), end='\n')
+        break
+    sock.settimeout(2.0)
+    packet = data[0]
+    packets.append(packet)
+    #print("Packets received:",len(packets), end='\r')
+    if len(packet) < 12:
+        continue
+
+#s.close()
+#print("Socket closed")
+
+
+for packet in packets:
+    fb, sb, seqnum, timestamp = struct.unpack("!BBHI", packet[:8])  # RTP header
+
+    pad = (fb >> 5) & 1
+    ext = (fb >> 4) & 1
+    cc = fb & 15
+    mark = (sb >> 7) & 1
+
+    if (pad > 3 or pad < 0):
+        continue
+
+    extsize = struct.unpack("!xxH", packet[12 + 4 * cc:16 + 4 * cc])[0] if ext else 0
+    padsize = 0
+    if pad == 1:
+        padsize = struct.unpack("!B", packet[-1:])[0]
+        # print(len(packet), padsize)
+    data = packet[12 + 4 * (cc + ext + extsize):-padsize] if pad else packet[12 + 4 * (cc + ext + extsize):]
+
+    if not len(data):
+        continue
+
+    nalheader = struct.unpack("!B", data[:1])[0]
+    nalf = (nalheader >> 7) & 1
+    nalnri = (nalheader >> 5) & 3
+    naltype = nalheader & 31
+    if 0 < naltype < 24:  # single unit
+        nals.append(data)
+    elif naltype == 24:  # stap-a
+        naldata = data[1:]
+        while len(naldata):
+            nalsize = struct.unpack("!H", naldata[:2])[0]
+            naldata = naldata[2:]
+            tdata = naldata[:nalsize]
+            nals.append(tdata)
+            naldata = naldata[nalsize:]
+    elif naltype == 28:  # fu-a
+        fuheader = struct.unpack("!xB", data[:2])[0]
+        s = fuheader >> 7
+        e = (fuheader >> 6) & 1
+        if s:
+            theader = (nalheader & 224) | (fuheader & 31)
+            # print ('theader: ', theader, end='\n\n')
+            fudata = struct.pack("!B", theader) + data[2:]
+            # print ('fudata0 ', fudata, end='\n\n')
+        else:
+            fudata += data[2:]
+            # print ('fudata1 ', fudata, end='\n\n')
+
+        if e:
+            # print ('fudata: ', fudata, end='\n\n')
+            nals.append(fudata)
+            # print ('nal: ', nals[len(nals)-1], end='\n\n')
+            # print('fudata: ', fudata)
+
+    else:
+        print('lol')
+        continue
+
+print('nals: ' + str(len(nals)), 'singles: ' + str(sum1), '; fus: ' + str(sum2))
+file2 = open('rawdata.h264', 'wb')
+rawnals = b''
+sum1 = 0
+print('stop')
+for nal in nals:
+    nal = struct.pack("!I", 1) + nal
+    file2.write(nal)
+    sum1 += len(nal)
+
+bi = struct.pack('!B', 100)
+stre = bi + struct.pack('!B', 0)
+bi2 = struct.unpack('!B', stre[0:1])[0]
+print(bi2)
+
+file2.close()
+# ffmpeg -f h264 -i rawdata.h264 -vcodec copy output.mp4
--- rtp.py
+++ rtp.py
@@ -1,131 +1,24 @@
-"""This is RTP Transmitter for H.264 Payload
+#!/usr/bin/env python3
+
+"""This is script for RTP transmission of H.264
 
 
 """
 
+import argparse
 
-import socket
-import struct
-import sys
-from math import ceil
-from random import getrandbits
-from fractions import Fraction as Frac
+from transmission import Transmitter
 
 
-class Transmitter:
+parser = argparse.ArgumentParser(description='Sending H.264 via UDP to specified address')
+parser.add_argument('-i', '--file', type=str, dest='File', default='test.h264')
+parser.add_argument('-m', '--mode', type=str, dest='Mode', default='single')
+parser.add_argument('-a', '--address', '--ip', type=str, dest='IP', default='127.0.0.1')
+parser.add_argument('-p', '--port', type=int, dest='Port', default=5000)
+parser.add_argument('-r', '--receive', dest='receive_flag', action='store_true', default=False)
+parser.add_argument('-t', '--transmit', dest='transmit_flag', action='store_true', default=False)
 
-    type_mask = 0b11111
-    nri_mask = 0b11100000
-    unit_types = {'FU-A': 28, 'FU-B': 29}
-    fu_a_type = 28
-    fu_b_type = 29
-    pt = {'FU-A': 99, 'single': 98}
-
-    def __init__(self, file: str = 'test.h264', address: tuple = ('127.0.0.1', 5000),
-                 mode: str = 'single', fps: int = 24, sn: int = getrandbits(16),
-                 timestamp: int = getrandbits(16), ssrc: bytes = getrandbits(16),
-                 cc: int = 0, v: int = 2, x: bool = 0, m: bool = 0):
-        self.address = address
-        self.socket_ = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.file = file
-        self.mode = mode
-        self.sn = sn
-        if not 1 < fps < 200:
-            print('Error: wrong frame rate')
-            sys.exit(2)
-        self.delay = Frac((1/fps)*90000)
-        self.timestamp = Frac(timestamp, self.delay.denominator)
-        self.ssrc = ssrc
-        self.cc = cc
-        self.v = v
-        self.x = x
-        self.m = m
-        self.packets_send = 0
-
-    @staticmethod
-    def check_prefix(bytestream):
-        if bytestream[0:4] == bytes((0, 0, 0, 1)):
-            return True
-        return False
-
-    def get_preferences_from_file(self):
-        pass
-
-    def make_header(self, p=0, **kwargs) -> bytes:
-        nal_header = bytes()
-        if kwargs['rtp_type'] == 'FU-A':
-            unit_type = kwargs['first_byte'] & self.type_mask
-            unit_nri = kwargs['first_byte'] & self.nri_mask
-            fu_header = unit_type
-            indicator = self.unit_types[kwargs['rtp_type']] | unit_nri
-            if kwargs['flag'] == 's':
-                fu_header = 0b10000000 | fu_header
-            elif kwargs['flag'] == 'e':
-                fu_header = 0b01000000 | fu_header
-            nal_header = struct.pack('!BB', indicator, fu_header)
-
-        first_byte = self.v << 6 | (p << 5) | (self.x << 4) | self.cc
-        second_byte = (self.m << 7) | self.pt[kwargs['rtp_type']]
-        rtp_header = struct.pack('!BBHII', first_byte, second_byte, self.sn, round(self.timestamp), self.ssrc)
-
-        header = rtp_header + nal_header
-
-        return header
-
-    # def send_packet(self,):
-
-    def send_unit(self, unit, rtp_type='single', **kwargs):
-        if rtp_type == 'single':
-            header = self.make_header(rtp_type=rtp_type, p=0)
-            self.socket_.sendto(header+unit, self.address)
-            self.packets_send += 1
-
-        elif rtp_type == 'FU-A':
-            unit_size = kwargs['unit_size']
-            n = ceil((len(unit)-1) / unit_size-2)
-            header = self.make_header(p=0, flag='s', first_byte=unit[0],
-                                      rtp_type=rtp_type)
-            self.socket_.sendto(header + unit[1:unit_size-1], self.address)
-            self.packets_send += 1
-            for i in range(1, n-1):
-                header = self.make_header(p=0, first_byte=unit[0], flag='m',
-                                          rtp_type=rtp_type)
-                self.socket_.sendto(header + unit[(unit_size-2)*i+1:(unit_size-2)*(i+1)+1], self.address)
-                self.packets_send += 1
-            header = self.make_header(p=0, first_byte=unit[0], rtp_type=rtp_type, flag='e')
-            self.socket_.sendto(header + unit[(unit_size-2)*(n-1)+1:], self.address)
-            self.packets_send += 1
-
-        self.sn = (self.sn + 1) % 2 ** 16
-        self.timestamp += self.delay
-        print(f'Unit {self.sn} send')
-
-    def transmit(self, mode: str = None, packet_size: int = 260) -> object:
-        try:
-            file = open(self.file, 'rb')
-        except FileNotFoundError:
-            print('File not found')
-            sys.exit(1)
-        bytestream = bytearray(file.read())
-        unit_size = None
-
-        if mode:
-            self.mode = mode
-
-        if self.mode == 'FU-A':
-            unit_size = packet_size - (12 + self.cc * 4)
-            if unit_size < 2:
-                print('Error: Too small packet_size')
-                sys.exit(2)
-
-        while len(bytestream) > 1:
-
-            end = bytestream.find(bytes((0, 0, 0, 1)), 1)
-            unit = bytestream[4:end]
-            self.send_unit(unit, self.mode, unit_size=unit_size)
-
-            del bytestream[:end]
-
-        print(f'While transmission {self.packets_send} packets were send')
-
-        return 0
+args = parser.parse_args()
+if args.transmit_flag:
+    T = Transmitter(args.File, (args.IP, args.Port), args.Mode)
+    T.transmit()
